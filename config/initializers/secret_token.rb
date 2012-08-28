dd77010848acd8fc7c8d90447cec5c006eccdfc0
Chip Jackson <chipjacks@gmail.com>
2012-08-28 10:43:40 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Runodo::Application.config.secret_token = 'e19746533c36eb34bac040274d5cf9de352dcbf6caf43d1660637e4f378595d3800c8cdfe12c10f4d0306322244cf68342cf76065ee7e8bdcd0f174a75820344'
