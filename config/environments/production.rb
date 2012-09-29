de4120cbfd11fd78b607b1c5fef70152744be9ed
Chip Jackson <chipjacks@gmail.com>
2012-09-29 13:01:54 -0700

    update production action mailer default url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,9 @@ Runodo::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
+  config.action_mailer.default_url_options = { :host => 'rhino-log.herokuapp.com' }
   config.action_mailer.perform_deliveries    = true
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.delivery_method = :smtp
 
