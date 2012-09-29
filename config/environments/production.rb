42442ad9107ca3e87ffe2ca554431d0a5d912327
Chip Jackson <chipjacks@gmail.com>
2012-09-29 12:55:14 -0700

    update mailer settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,18 @@ Runodo::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  config.action_mailer.perform_deliveries    = true
+
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    :enable_starttls_auto => true,
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :domain => "gmail.com",
+    :authentication => :login,
+    :user_name => "rhinolognet@gmail.com",
+    :password => "rhinolognet",
+  }
 end
