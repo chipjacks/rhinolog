9a3681a39cb923a35b2fd108a68373e28e7bc67b
Chip Jackson <chipjacks@gmail.com>
2012-12-26 10:17:51 -0800

    update text message parsing

--- app/controllers/receive_text_controller.rb
+++ app/controllers/receive_text_controller.rb
@@ -6,25 +6,34 @@ class ReceiveTextController < ApplicationController
     @from_number = params["From"].slice(2..11)
 
 		if !(dude = User.find_by_phone(@from_number))
-    	@response = "Account not found, please register your phone number at rhinolog.net"
+    	@response = "Account not found, please register your phone number at Rhinolog.net"
     else
     	run_data = parse_body(@message_body)
 
     	newrun = dude.runs.build(run_data[:data])
 
-	    if newrun.valid?
-	    	newrun.save!
-		    @response = "Logged succesfully - date: #{newrun.date.strftime("%-m/%d")}, distance: #{sprintf("%g", newrun.distance)}, time: #{ChronicDuration.output(newrun.time_in_secs, :format => :short)}, pace: #{ChronicDuration.output(newrun.pace_in_secs, :format => :short)}"
-		  else
-		  	@response = "Unable to log run. "
-		  	if run_data[:errors].empty?
-		  		@response += "Not enough data: need data for two of distance, time, and pace. Example: 'date 9/25, distance 12, pace 7m30s, feel good, effort easy'"
-		  	else
-		  		@response += "Unable to parse "
-		  		@response += run_data[:errors].join(", ")
-		  		@response += "."
-		  	end
-		  end
+    	if run_data[:errors].empty?
+    		newrun.save!
+    		@response = "Logged succesfully - date: #{newrun.date.strftime("%-m/%d")}, distance: #{sprintf("%g", newrun.distance)}, time: #{ChronicDuration.output(newrun.time_in_secs, :format => :short)}, pace: #{ChronicDuration.output(newrun.pace_in_secs, :format => :short)}"
+    	else
+    		@response = "Unable to log run. Couldn't parse "
+    		@response += run_data[:errors].join(", ")
+		  	@response += "."
+	    end
+
+	   #  if newrun.valid?
+	   #  	newrun.save!
+		  #   @response = "Logged succesfully - date: #{newrun.date.strftime("%-m/%d")}, distance: #{sprintf("%g", newrun.distance)}, time: #{ChronicDuration.output(newrun.time_in_secs, :format => :short)}, pace: #{ChronicDuration.output(newrun.pace_in_secs, :format => :short)}"
+		  # else
+		  # 	@response = "Unable to log run. "
+		  # 	if run_data[:errors].empty?
+		  # 		@response += "Not enough data: need data for two of distance, time, and pace. Example: 'date 9/25, distance 12, pace 7m30s, feel good, effort easy'"
+		  # 	else
+		  # 		@response += "Unable to parse "
+		  # 		@response += run_data[:errors].join(", ")
+		  # 		@response += "."
+		  # 	end
+		  # end
     end
 
 	  respond_to do |format|
